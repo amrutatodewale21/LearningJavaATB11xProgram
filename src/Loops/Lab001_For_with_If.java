e5c2bd07feecea78a95bb7f828c72e6ae6c33edc
Amruta <amrutatodewale21@gmail.com>
2025-03-20 01:40:40 +0530

    )- march_Initial Push

---
+++ src/Loops/Lab001_For_with_If.java
@@ -0,0 +1,24 @@
+package Loops;
+
+public class Lab001_For_with_If {
+    public static void main(String[] args) {
+        for(int i=0;i<18;i++)
+        {
+            if(i>15)
+            {
+                System.out.println("Print1");
+            }
+            else
+            {
+                System.out.println("Print2");
+            }
+        }
+    }
+
+
+
+}
+
+
+//16 times it will print Print2
+//2 times it will print Print1
---
+++ src/Loops/Lab002_Switch.java
@@ -0,0 +1,27 @@
+package Loops;
+
+public class Lab002_Switch {
+    public static void main(String[] args) {
+        String name=args[0];
+//        String name ="Rekha";
+        switch (name)
+        {
+            case "swarupa" :
+                System.out.println("Invalid name");
+                break;
+            case "Amruta" :
+                System.out.println("Invalid name");
+                break;
+            case "Rekha" :
+                System.out.println("now its valid name");
+                break;
+            default:
+                System.out.println("invalid string");
+
+    }
+
+
+    }
+
+
+}
